06705e5072645407425a0beefce1a6865a48de33
Foxxx48 <Foxxdim@mail.ru>
2023-06-01 12:02:56 +0300

    fixed VKNewsMainScreen added FAB and this state

--- app/src/main/java/com/example/smcardcompose/ui/VKNewsMainScreen.kt
+++ app/src/main/java/com/example/smcardcompose/ui/VKNewsMainScreen.kt
@@ -4,26 +4,63 @@ package com.example.smcardcompose.ui
 import android.annotation.SuppressLint
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.SnackbarDuration
+import androidx.compose.material.SnackbarHost
+import androidx.compose.material.SnackbarHostState
+import androidx.compose.material.SnackbarResult
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.smcardcompose.ui.theme.SMCardComposeTheme
+import kotlinx.coroutines.launch
 
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MainScreen() {
+    val snackbarHostState = SnackbarHostState()
+    val scope = rememberCoroutineScope()
+    val fabIsVisible = remember {
+        mutableStateOf(true)
+    }
+
     Scaffold(
+        snackbarHost = {
+            SnackbarHost(snackbarHostState)
+        },
+        floatingActionButton = {
+            if (fabIsVisible.value) {
+                FloatingActionButton(
+                    onClick = {
+                        scope.launch {
+                            val action = snackbarHostState.showSnackbar(
+                                "SnackBarScope",
+                                actionLabel = "Hide FAB",
+                                duration = SnackbarDuration.Long
+                            )
+                            if (action == SnackbarResult.ActionPerformed) {
+                                fabIsVisible.value = false
+                            }
+                        }
+                    }
+                ) {
+                    Icon(Icons.Filled.Add, contentDescription = null)
+                }
+            }
+
+        },
         bottomBar = {
-            BottomNavigation(
-//                backgroundColor = MaterialTheme.colorScheme.background
-            ) {
+            BottomNavigation() {
                 val selectedItemPosition = remember {
                     mutableStateOf(0)
                 }
@@ -37,7 +74,7 @@ fun MainScreen() {
                 items.forEachIndexed() { index, item ->
                     BottomNavigationItem(
                         selected = selectedItemPosition.value == index,
-                        onClick = {selectedItemPosition.value = index},
+                        onClick = { selectedItemPosition.value = index },
                         icon = {
                             Icon(
                                 item.icon, contentDescription = null,
@@ -47,22 +84,16 @@ fun MainScreen() {
                         label = {
                             Text(
                                 text = stringResource(id = item.titleResId),
-//                                color = MaterialTheme.colorScheme.onPrimary
                             )
                         },
                         selectedContentColor = Color.Red,
-                        unselectedContentColor = Color.Green
+                        unselectedContentColor = Color.Green,
                     )
                 }
             }
-
         },
-
-        ) {
-
+    ) {
     }
-
-
 }
 
 @Preview
@@ -72,7 +103,6 @@ fun PreviewLightTheme() {
         darkTheme = false,
         dynamicColor = false
     ) {
-
         MainScreen()
     }
 }
@@ -85,7 +115,6 @@ fun PreviewDarkTheme() {
         dynamicColor = false
     ) {
         MainScreen()
-
     }
 }
 
