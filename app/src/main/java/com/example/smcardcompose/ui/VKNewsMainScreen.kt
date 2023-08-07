0a1e6213a160832423fdc6100c1df466f56bca77
Foxxx48 <Foxxdim@mail.ru>
2023-08-07 16:58:14 +0300

    fixed colorScheme

--- app/src/main/java/com/example/smcardcompose/ui/VKNewsMainScreen.kt
+++ app/src/main/java/com/example/smcardcompose/ui/VKNewsMainScreen.kt
@@ -2,18 +2,19 @@ package com.example.smcardcompose.ui
 
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,7 +28,9 @@ fun MainScreen(viewModel: MainViewModel) {
 
     Scaffold(
         bottomBar = {
-            BottomNavigation() {
+            BottomNavigation(
+
+            ) {
                 val selectedItemPosition = remember {
                     mutableStateOf(0)
                 }
@@ -41,6 +44,8 @@ fun MainScreen(viewModel: MainViewModel) {
 
                 items.forEachIndexed() { index, item ->
                     BottomNavigationItem(
+                        modifier = Modifier
+                            .background(color = MaterialTheme.colorScheme.onSecondary),
                         selected = selectedItemPosition.value == index,
                         onClick = { selectedItemPosition.value = index },
                         icon = {
@@ -54,8 +59,8 @@ fun MainScreen(viewModel: MainViewModel) {
                                 text = stringResource(id = item.titleResId),
                             )
                         },
-                        selectedContentColor = Color.Red,
-                        unselectedContentColor = Color.Green,
+                        selectedContentColor = MaterialTheme.colorScheme.onPrimary,
+                        unselectedContentColor = MaterialTheme.colorScheme.background,
                     )
                 }
             }
